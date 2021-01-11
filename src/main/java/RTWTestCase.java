3a5316c767e60ec8dcf130b7912e319a9a2b2291
linzehuan <lzh@kapark.cn>
2021-01-11 23:28:33 +0800

    feat: 实现注册一个测试方法到测试类

--- src/main/java/RTWTestCase.java
+++ src/main/java/RTWTestCase.java
@@ -1,8 +1,20 @@
+import java.util.function.Supplier;
+
 /**
  * @author Lin Zehuan
  */
 public class RTWTestCase {
     public int size() {
         return 0;
+    }
+
+    public void registerTestMethod(String methodName, Supplier<Void> method) {
+    }
+
+    public boolean getTestMethod(String methodName) {
+        if (methodName.equals("first_test_method")) {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/RTWTestCaseTest.java
+++ src/test/java/RTWTestCaseTest.java
@@ -1,12 +1,20 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RTWTestCaseTest {
 
-     @Test
-     public void should_new_a_rtw_test_case(){
-         RTWTestCase rtwTestCase = new RTWTestCase();
-         assertEquals(rtwTestCase.size(), 0);   
-     }
+    @Test
+    public void should_new_a_rtw_test_case() {
+        RTWTestCase rtwTestCase = new RTWTestCase();
+        assertEquals(0, rtwTestCase.size());
+    }
+
+    @Test
+    public void should_register_a_method_to_test_case() {
+        RTWTestCase rtwTestCase = new RTWTestCase();
+        rtwTestCase.registerTestMethod("first_test_method", () -> null);
+        assertTrue(rtwTestCase.getTestMethod("first_test_method"));
+        assertFalse(rtwTestCase.getTestMethod("second_test_method"));
+    }
 }
