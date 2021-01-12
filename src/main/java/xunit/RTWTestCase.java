a71087fb05781774032c427e04661896ee39e29f
linzehuan <lzh@kapark.cn>
2021-01-12 17:51:38 +0800

    refactor: 消除魔法值，增加TestMethod

--- src/main/java/xunit/RTWTestCase.java
+++ src/main/java/xunit/RTWTestCase.java
@@ -1,13 +1,23 @@
 package xunit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Lin Zehuan
  */
 public class RTWTestCase {
 
+    private List<TestMethod> testMethods = new ArrayList<>();
+
+    public RTWTestCase() {
+        if (this instanceof SingleMethodTestCase) {
+            this.testMethods.add(new TestMethod());
+        }
+    }
     public int getTestMethodsCount() {
         if (this instanceof SingleMethodTestCase) {
-            return 1;
+            return testMethods.size();
         }
         return 2;
     }
---
+++ src/main/java/xunit/TestMethod.java
@@ -0,0 +1,7 @@
+package xunit;
+
+/**
+ * @author Lin Zehuan
+ */
+public class TestMethod {
+}
