35b5492d790372a977722e7750f92af252a06121
linzehuan <lzh@kapark.cn>
2021-01-12 18:44:32 +0800

    refactor: 消除RTWTestCase里的hardcode

--- src/main/java/xunit/DoubleMethodTestCase.java
+++ src/main/java/xunit/DoubleMethodTestCase.java
@@ -5,4 +5,16 @@ package xunit;
  */
 public class DoubleMethodTestCase extends RTWTestCase{
 
+    public DoubleMethodTestCase() {
+        this.registerTestMethod("first_test_method",this::first_test_method);
+        this.registerTestMethod("second_test_method",this::second_test_method);
+    }
+
+    public void second_test_method() {
+
+    }
+
+    public void first_test_method() {
+
+    }
 }
--- src/main/java/xunit/RTWTestCase.java
+++ src/main/java/xunit/RTWTestCase.java
@@ -11,16 +11,13 @@ public class RTWTestCase {
     private List<TestMethod> testMethods = new ArrayList<>();
 
     public RTWTestCase() {
-        if (this instanceof DoubleMethodTestCase) {
-            this.testMethods.add(new TestMethod("",()->{}));
-            this.testMethods.add(new TestMethod("",()->{}));
-        }
+ 
     }
     public int getTestMethodsCount() {
         return testMethods.size();
     }
 
     protected void registerTestMethod(String methodName, Runnable method) {
-        this.testMethods.add(new TestMethod(methodName, method));
+        this.testMethods.add(TestMethod.of(methodName, method));
     }
 }
